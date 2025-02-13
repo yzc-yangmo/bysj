51787efdc08cc9ba99d3b0f3d7452eb181e4dc89
yzc-yangmo <2403577910@qq.com>
2025-02-13 11:11:07 +0800

    define dataset

--- src/data/transform.py
+++ src/data/transform.py
@@ -5,26 +5,24 @@ from torchvision import transforms
 from torch.utils.data import Dataset
 
 class FoodImageTransform:
-    # 根据transform_code返回对应的transform
-    def __init__(self, transform_code=0):
-        self.transform_code = transform_code
 
-    def __call__(self):
+    # 根据transform_code返回对应的transform
+    def __init__(self, transform_type=0):
         
-        # 方案0：统一缩放到512x512  
-        if self.transform_code == 0:
-            return transforms.Compose([
-            transforms.Resize((512, 512)),
-            transforms.ToTensor()
+        if transform_type == 0:
+            self.transform = transforms.Compose([
+                transforms.Resize((512, 512)),
+                transforms.ToTensor()
             ])
-        
-        # 方案1：保持长宽比，短边填充
-        elif self.transform_code == 1:
-            return transforms.Compose([
-            transforms.Resize(512),  # 短边缩放到512
-            transforms.CenterCrop((512, 512)),
-            transforms.ToTensor()
+        elif transform_type == 1:
+            self.transform = transforms.Compose([
+                transforms.Resize(512),
+                transforms.CenterCrop((512, 512)),
+                transforms.ToTensor()
             ])
-        
         else:
             raise ValueError('Invalid transform code')
+    
+    def __call__(self, image):  # 接收图像参数
+        return self.transform(image)  # 直接转换图像
+        
