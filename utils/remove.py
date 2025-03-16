4bd2b6e7a00b126e77bb85131e1914b0d2648525
yzc-yangmo <2403577910@qq.com>
2025-03-16 22:57:43 +0800

    modift fiel structure

--- utils/remove.py
+++ utils/remove.py
@@ -1,7 +1,15 @@
 import os
 
+def remove_not_jpg(tragrt_path):
+    count = 0
+    for f in os.listdir(tragrt_path):
+        if not f.endswith(".jpg"):
+            os.remove(os.path.join(tragrt_path, f))
+            count += 1
+            print(f"count: {count} {f} remove!")
+    if count == 0:
+        print("All files's type is jpg!")
+
 if __name__ == '__main__':
-    for i in os.listdir("./"):
-        if i.endswith(".jpg"):
-            os.remove(i)
-            print(f"{i} remove successfully!")
+    target_path = input("Enter the target path: ")
+    remove_not_jpg(target_path)
