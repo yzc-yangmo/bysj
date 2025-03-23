b42dfdb50b781a1a6d9ce7f53798f1ff3d29fb7e
yzc-yangmo <2403577910@qq.com>
2025-03-23 11:37:27 +0800

    .

--- src/data/transform.py
+++ src/data/transform.py
@@ -16,7 +16,7 @@ class FoodImageTransform:
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
         
-        # 添加数据增强方案
+        # 一般数据增强方案
         elif transform_type == 1:
             self.transform = transforms.Compose([
                 transforms.RandomResizedCrop(256),
