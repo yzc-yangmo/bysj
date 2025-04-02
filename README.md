063f924216c8e8271155d31a703d48631c4e06db
yzc-yangmo <2403577910@qq.com>
2025-04-02 18:18:10 +0800

    finish v1

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # 食物识别系统
 
-## 获取数据集
+## 数据集
 
-第一部分食物图像：[Hugging Face链接](https://huggingface.co/datasets/ethz/food101)
+食物图像数据集使用 Hugging Face 上的公开数据集 [food-chinese-2017](https://huggingface.co/datasets/chaeso/food_chinese_2017)
 
-图像为 `.parquet`格式，写了一个脚本 **parquet_to_img.py** 转换为 jpg 格式（图片原格式为 jpg）
+共 256 个食物类别，每个类别大约 200 张图片，图像尺寸为 256x256。
+
+## 模型
+
+模型使用 Vision Transformer 模型。
+
+## 训练
+
+训练使用 128 的批量大小，使用 1000 的训练轮数，使用 0.01 的权重衰减，使用 5e-4 的学习率。
+
+```bash
+cd src
+python/python3 train.py
+```
+
+## 推理
+
+```bash
+python main.py
+```
+
--- src/gui.py
+++ src/gui.py
@@ -1,4 +1,4 @@
-import os, json
+import os, json, time
 import torch
 import csv
 from PyQt5.QtWidgets import (QMainWindow, QWidget, QVBoxLayout, 
@@ -6,6 +6,7 @@ from PyQt5.QtWidgets import (QMainWindow, QWidget, QVBoxLayout,
                             QScrollArea, QGridLayout, QGroupBox, QStatusBar)
 from PyQt5.QtGui import QPixmap,QFont, QIcon
 from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication
 from InferenceEngine import InferenceEngine
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -18,6 +19,7 @@ class FoodRecognitionSystem(QMainWindow):
         self.initUI()
         self.load_model()
         self.first_upload = True # 第一次上传完成后，将first_upload设置为False，再次上传时无需点击即可进行识别
+        self.type = "single" # 识别类型，single: 单张图片识别，dir: 文件夹批量识别
 
     # 创建推理实例
     def load_model(self):
@@ -85,16 +87,16 @@ class FoodRecognitionSystem(QMainWindow):
         
         self.upload_btn = QPushButton('上传图片')
         self.upload_btn.clicked.connect(self.upload_image)
-        self.upload_dir_btn = QPushButton('上传文件夹')
-        self.upload_dir_btn.clicked.connect(self.upload_directory)
+        self.upload_dir_btn = QPushButton('上传文件夹/批量识别')
+        self.upload_dir_btn.clicked.connect(self.upload_dir)
         self.recognize_btn = QPushButton('识别食物')
         self.recognize_btn.clicked.connect(self.recognize_food)
         self.clear_btn = QPushButton('清除')
         self.clear_btn.clicked.connect(self.clear_all)
         
         control_layout.addWidget(self.upload_btn, 0, 0)
-        control_layout.addWidget(self.upload_dir_btn, 1, 0)
-        control_layout.addWidget(self.recognize_btn, 0, 1)
+        control_layout.addWidget(self.upload_dir_btn, 0, 1)
+        control_layout.addWidget(self.recognize_btn, 1, 0)
         control_layout.addWidget(self.clear_btn, 1, 1)
         
         control_group.setLayout(control_layout)
@@ -121,38 +123,35 @@ class FoodRecognitionSystem(QMainWindow):
         self.statusBar = QStatusBar()
         self.setStatusBar(self.statusBar)
         self.statusBar.showMessage('准备就绪')
-        
+    
+    # 在界面中显示图片
+    def show_image(self, image_path):
+        """显示图片"""
+        pixmap = QPixmap(image_path)
+        pixmap = pixmap.scaled(self.image_display.width(), 
+                              self.image_display.height(),
+                              Qt.KeepAspectRatio, 
+                              Qt.SmoothTransformation)
+        self.image_display.setPixmap(pixmap)
+    
     def upload_image(self):
         """上传图片功能"""
         file_path, _ = QFileDialog.getOpenFileName(self, '选择图片', '', 
                                                   'Images (*.png *.jpg *.jpeg)')
         if file_path:
-            pixmap = QPixmap(file_path)
-            if not pixmap.isNull():
-                # 调整图片大小以适应显示区域，保持纵横比
-                pixmap = pixmap.scaled(self.image_display.width(), 
-                                      self.image_display.height(),
-                                      Qt.KeepAspectRatio, 
-                                      Qt.SmoothTransformation)
-                self.image_display.setPixmap(pixmap)
-                self.statusBar.showMessage(f'已加载图片: {file_path}')
-                self.current_image_path = file_path
+            self.show_image(file_path)
+            self.statusBar.showMessage(f'已加载图片: {file_path}')
+            self.current_image_path = file_path
+            self.type = "single" # 设置识别类型为单张图片识别
+            
+            # 如果不是第一次上传完成后，则直接进行识别
+            if not self.first_upload:
+                self.recognize_single()
                 
-                if not self.first_upload:
-                    self.recognize_food()
-                
-            else:
-                self.statusBar.showMessage('无法加载图片')
-    
-    def upload_directory(self):
-        """上传文件夹功能"""
-        dir_path = QFileDialog.getExistingDirectory(self, '选择文件夹')
-        if dir_path:
-            self.current_dir_path = dir_path
-            self.statusBar.showMessage(f'已选择文件夹: {dir_path}')
-            self.recognize_directory()
+        else:
+            self.statusBar.showMessage('无法加载图片')
 
-    def recognize_food(self):
+    def recognize_single(self):
         """识别食物功能"""
         if hasattr(self, 'current_image_path'):
             self.statusBar.showMessage('正在识别...')
@@ -193,7 +192,15 @@ class FoodRecognitionSystem(QMainWindow):
         else:
             self.statusBar.showMessage('请先上传图片')
     
-    def recognize_directory(self):
+    def upload_dir(self):
+        """上传文件夹功能"""
+        dir_path = QFileDialog.getExistingDirectory(self, '选择文件夹')
+        if dir_path:
+            self.current_dir_path = dir_path
+            self.statusBar.showMessage(f'已选择文件夹: {dir_path}')
+            self.type = "dir" # 设置识别类型为文件夹批量识别
+    
+    def recognize_dir(self):
         """批量识别文件夹中的图片"""
         if not hasattr(self, 'current_dir_path'):
             self.statusBar.showMessage('请先选择文件夹')
@@ -211,10 +218,13 @@ class FoodRecognitionSystem(QMainWindow):
             return
 
         # 准备CSV文件
-        csv_path = os.path.join(self.current_dir_path, 'recognition_results.csv')
+        csv_path = f'recognition-results-{time.strftime("%Y%m%d%H%M%S")}.csv'
         csv_headers = ['图片名称', '食物名称', '置信度', '识别耗时(秒)', 
                       '热量(kcal)', '蛋白质(g)', '脂肪(g)', '碳水化合物(g)']
 
+        # 记录开始时间
+        start_time = time.time()
+        
         try:
             with open(csv_path, 'w', newline='', encoding='utf-8-sig') as csvfile:
                 writer = csv.writer(csvfile)
@@ -222,8 +232,15 @@ class FoodRecognitionSystem(QMainWindow):
                 
                 total_images = len(image_files)
                 for i, image_file in enumerate(image_files, 1):
+                    
                     image_path = os.path.join(self.current_dir_path, image_file)
-                    self.statusBar.showMessage(f'正在处理: {image_file} ({i}/{total_images})')
+                    self.statusBar.showMessage(f'processing: {image_file} ({i}/{total_images})')
+                    
+                    # 处理UI事件，使界面能够更新
+                    QApplication.processEvents()
+                    
+                    # 在界面中显示图片
+                    self.show_image(image_path)
                     
                     # 进行推理
                     infer_info = self.InferenceEngine_engine.inference(image_path)
@@ -231,27 +248,54 @@ class FoodRecognitionSystem(QMainWindow):
                     if infer_info["success"]:
                         writer.writerow([
                             image_file,
-                            infer_info["food_info"]["chn"],
+                            infer_info["food_name"],
                             f"{infer_info['confidence']:.1f}%",
                             f"{infer_info['inference_time']:.2f}",
-                            infer_info["food_info"]["calories"],
-                            infer_info["food_info"]["protein"],
-                            infer_info["food_info"]["fat"],
-                            infer_info["food_info"]["carb"]
+                            infer_info["nutrition_info"]["calories"],
+                            infer_info["nutrition_info"]["protein"],
+                            infer_info["nutrition_info"]["fat"],
+                            infer_info["nutrition_info"]["carb"]
                         ])
+                        # 更新识别结果
+                        result_text = f"""
+                        <p><b>进度:</b> {i}/{total_images}</p>
+                        <p><b>图片名称:</b> {image_file}</p>
+                        <p><b>食物名称:</b> {infer_info["food_name"]}</p>
+                        <p><b>置信度:</b> {infer_info["confidence"]:.1f}%</p>
+                        <p><b>耗时:</b> {infer_info["inference_time"]:.2f} 秒</p>
+                        <p><br></p>
+                        <p><b>营养成分:</b></p>
+                        <p><b>热量:</b> {infer_info["nutrition_info"]["calories"]} kcal</p>
+                        <p><b>蛋白质:</b> {infer_info["nutrition_info"]["protein"]} g</p>
+                        <p><b>脂肪:</b> {infer_info["nutrition_info"]["fat"]} g</p>
+                        <p><b>碳水化合物:</b> {infer_info["nutrition_info"]["carb"]} g </p>
+                        <p><b>以上数据为100g食物的营养成分</b></p>
+                        """
+                        self.result_label.setText(result_text)
                     else:
                         writer.writerow([
                             image_file,
                             f"识别失败: {infer_info['error']}",
                             "", "", "", "", "", ""
                         ])
+                    
+                    # 再次处理UI事件，确保每次推理后界面都能更新
+                    QApplication.processEvents()
 
-            self.statusBar.showMessage(f'批量识别完成，结果已保存至: {csv_path}')
-            self.result_label.setText(f'批量识别完成，共处理 {total_images} 张图片\n结果已保存至: {csv_path}')
+            self.statusBar.showMessage(f'批量识别完成，结果已保存至: {os.path.abspath(csv_path)}')
+            self.result_label.setText(f'批量识别完成!\n共处理 {total_images} 张图片\n耗时: {time.time() - start_time:.2f} 秒\n结果已保存至: {os.path. abspath(csv_path)}')
             
         except Exception as e:
             self.statusBar.showMessage(f'批量识别过程中发生错误: {str(e)}')
             self.result_label.setText(f"批量识别过程中发生错误: {str(e)}")
+    
+    # 识别食物功能，根据识别类型选择识别方式
+    def recognize_food(self):
+        """识别食物功能"""
+        if self.type == "single":
+            self.recognize_single()
+        elif self.type == "dir":
+            self.recognize_dir()
     
     def clear_all(self):
         """清除所有内容"""
